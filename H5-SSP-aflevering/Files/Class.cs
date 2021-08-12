5f61079aeb69d04364dde7310e23dbdbb36dd7e9
ShishaQQ <Linette-Kampmann@hotmail.dk>
2021-08-12 08:49:56 +0200

    sat test classes ind så andreas har adgang til mapperne

---
+++ H5-SSP-aflevering/Files/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace H5_SSP_aflevering.Files
+{
+    public class Class
+    {
+    }
+}
