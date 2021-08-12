581ea908c57853d5e0122bd45182c2a838b060e2
Krulsnar <32385513+Krulsnar@users.noreply.github.com>
2021-08-12 08:33:09 +0200

    Add project files.

---
+++ H5-SSP-aflevering/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H5_SSP_aflevering.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
