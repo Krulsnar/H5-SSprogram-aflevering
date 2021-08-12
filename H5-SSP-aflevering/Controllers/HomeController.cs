ab07f2f50f1c685e68c781f8c97fca0a00a6f18f
Krulsnar <32385513+Krulsnar@users.noreply.github.com>
2021-08-12 10:17:37 +0200

    Encrypt/Decrypt

--- H5-SSP-aflevering/Controllers/HomeController.cs
+++ H5-SSP-aflevering/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using H5_SSP_aflevering.Models;
+﻿using H5_SSP_aflevering.Code;
+using H5_SSP_aflevering.Models;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,15 +14,33 @@ namespace H5_SSP_aflevering.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IDataProtector _dataProtector;
+        private readonly Encryption _encryption;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            IDataProtectionProvider dataProtector,
+            Encryption encryption
+            )
         {
             _logger = logger;
+            _dataProtector = dataProtector.CreateProtector("testKey");
+            _encryption = encryption;
         }
 
         public IActionResult Index()
         {
-            return View();
+            string input = "Hello Andreas og Linette";
+            string encryptedInput = _encryption.Encrypt(input, _dataProtector);
+            string decryptedInput = _encryption.Decrypt(encryptedInput, _dataProtector);
+
+            IndexModel inputModel = new IndexModel()
+            {
+                EncryptedDescription = encryptedInput,
+                DecryptedDescription = decryptedInput
+            };
+            
+            return View(model: inputModel);
         }
 
         public IActionResult Privacy()
---
+++ H5-SSP-aflevering/Models/IndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace H5_SSP_aflevering.Models
+{
+    public class IndexModel
+    {
+        public string EncryptedDescription { get; set; }
+        public string DecryptedDescription { get; set; }
+    }
+}
