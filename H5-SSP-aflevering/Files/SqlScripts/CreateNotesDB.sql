0e1ab49a8065cf4ca0a5fcb63e577cbc6beb6938
Krulsnar <32385513+Krulsnar@users.noreply.github.com>
2021-08-12 12:26:44 +0200

    Create notes DB

---
+++ H5-SSP-aflevering/Files/SqlScripts/CreateNotesDB.sql
@@ -0,0 +1,12 @@
+﻿Create database H5_SSP_TODO
+go
+
+use H5_SSP_TODO
+go
+
+create table Notes(
+Id int identity(1,1) Primary Key NOT NULL,
+UserId nvarchar(MAX) NOT NULL,
+Title nvarchar(MAX) NOT NULL,
+Note nvarchar(MAX) NOT NULL
+);
--- H5-SSP-aflevering/Files/SqlScripts/CreateNotesTable.sql
+++
@@ -1,7 +0,0 @@
-﻿use H5_SSP
-
-create table Notes(
-Id int identity(1,1) Primary Key not null,
-AspNetUsersId nvarchar(450) foreign key references AspNetUsers(id) not null,
-Notes nvarchar(1000) not null
-);
