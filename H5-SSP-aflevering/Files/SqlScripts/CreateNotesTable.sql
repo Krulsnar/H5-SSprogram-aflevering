e23dd74d0a30b7997c3c7fadd4ae0c34fef4670d
Krulsnar <32385513+Krulsnar@users.noreply.github.com>
2021-08-12 12:08:22 +0200

    Certificat

--- H5-SSP-aflevering/Files/SqlScripts/CreateNotesTable.sql
+++ H5-SSP-aflevering/Files/SqlScripts/CreateNotesTable.sql
@@ -1,4 +1,6 @@
-﻿create table Notes(
+﻿use H5_SSP
+
+create table Notes(
 Id int identity(1,1) Primary Key not null,
 AspNetUsersId nvarchar(450) foreign key references AspNetUsers(id) not null,
 Notes nvarchar(1000) not null
