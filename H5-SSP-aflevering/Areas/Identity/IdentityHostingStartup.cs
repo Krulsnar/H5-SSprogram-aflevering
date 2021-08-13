9002443e66db2b197b684e7b3ebf5d714e5a6128
Krulsnar <32385513+Krulsnar@users.noreply.github.com>
2021-08-13 08:38:31 +0200

    Added login html page

---
+++ H5-SSP-aflevering/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using H5_SSP_aflevering.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(H5_SSP_aflevering.Areas.Identity.IdentityHostingStartup))]
+namespace H5_SSP_aflevering.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
